ccb78fae751fc7ce9795bb1e15a6c3f53a29b539
Jerome <jerome.neupert@epitech.eu>
2024-02-14 15:16:31 +0100

    Working Burger Button

---
+++ burger/script.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("test");
+let burgerNewAsset = document.querySelector(".burgerNewAsset");
+
+let isBurgerOpen = false;
+
+btn.addEventListener("click", function() {
+    if (isBurgerOpen) {
+        burgerNewAsset.classList.remove("burgerNewAssetVisible");
+        isBurgerOpen = false;
+        return;
+    }else {
+        burgerNewAsset.classList.add("burgerNewAssetVisible");
+        isBurgerOpen = true;
+        return;
+    }
+})
